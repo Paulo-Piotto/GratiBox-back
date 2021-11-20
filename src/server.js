0f71fb57e5b41bcd35063e956f82766f54f442b7
Paulo <paulo_piotto@hotmail.com>
2021-11-20 14:21:10 -0300

    corrigindo deploy

--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@
 import './setup.js';
 import app from './app.js';
 
-if (process.env.process.env.NODE_ENV === 'test') {
-  const port = 4000;
+// const port = 4000;
 
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}.`);
-  });
-} else {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}.`);
+// });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
