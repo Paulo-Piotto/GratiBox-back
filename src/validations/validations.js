541ac30464c3284ff2b3ffe3b590b8a701399aca
Paulo <paulo_piotto@hotmail.com>
2021-11-20 09:27:30 -0300

    objeto de validação de entrada finalizado

--- src/validations/validations.js
+++ src/validations/validations.js
@@ -11,7 +11,16 @@ const registerValidation = joi.object({
   password: joi.string().alphanum().min(5).required(),
 });
 
+const planValidation = joi.object({
+  user_id: joi.string().min(3).max(30).required(),
+  type: joi.string().min(6).required(),
+  deleivery_date: joi.date().required(),
+  products: joi.string().min(4).required(),
+  sign_date: joi.date().required(),
+});
+
 export {
   loginValidation,
   registerValidation,
+  planValidation,
 };
